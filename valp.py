d89ca27d7121895f8883a1e9f1936daedabdae01
Claire Beamer <clairebea@protonmail.com>
2019-05-16 21:23:20 -0600

    Validate IPv4 addresses

--- valp.py
+++ valp.py
@@ -1,2 +1,13 @@
 import socket
-def validateIPs(input):
+
+# Stolen from https://stackoverflow.com/questions/319279/how-to-validate-ip-address-in-python
+def validateIPv4(input):
+    try:
+        socket.inet_pton(socket.AF_INET, address)
+    except:
+        return False
+    
+    return True
+    
+def validateIPv6(input):
+    return False
