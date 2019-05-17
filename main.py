962f4e7bdd153aaa654aad8ffbec77f5b654e733
Claire Beamer <clairebea@protonmail.com>
2019-05-16 21:50:09 -0600

    Valid endpoint creation and some error checking.

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def main():
     parser.add_argument("--test", help="Checks if the given URL and port can \
                         be reached and outputs some helpful data" )
     # Set the es port we will be using, default of 9200
-    parser.add_argument("--http-port", help="The HTTP port of the \
+    parser.add_argument("--port", help="The HTTP port of the \
                          Elasticsearch host. Defaults to 9200", \
                         action="store_true")
     
@@ -52,12 +52,13 @@ def main():
     if args.addr:
         argdict['addr'] = args.addr
     else:
-        argdict['addr'] = "localhost"
+        argdict['addr'] = "127.0.0.1"
 
-    if args.http-port:
-        argdict['http-port'] = args.http-port
+    if args.port:
+        argdict['port'] = args.port
     else:
-        argdict['http-port'] = 9200
+        argdict['port'] = 9200
+
     endpoint = endPointConstructor(argdict)
 
     print(endpoint)
@@ -66,14 +67,14 @@ def main():
 def endPointConstructor(argdict):
     # Currently Elasticsearch only supports API calls 
     # over http.
-    baseProto = "http"
-    if type(argdict) == "<class 'dict'>":
-        pass
-    else:
+    baseProto = "http://"
+    try:
+        argdict['port']
+    except TypeError:
         print("Something went wrong. The parameters were incorrectly formatted")
         sys.exit(1)
 
-    validIP = validateIPs(argdict['addr'])
+    validIP = validateIPv4(argdict['addr'])
 
     if validIP == True:
         baseEndpoint = baseProto + argdict['addr']
@@ -82,16 +83,16 @@ def endPointConstructor(argdict):
         print("Exiting")
         sys.exit(1)
     
-    if argdict['http-port'] == 9200:
-        pass
-    elif argdict['http-port'] <= 1024: 
+    if argdict['port'] == 9200:
+        baseEndpoint = baseEndpoint + ":" + str(argdict['port'])
+    elif argdict['port'] <= 1024: 
         # Log warning the Elasticsearch port we're connecting to 
         # conflicts with other ports.
-        print("Warning: 'http-port' uses a well-known port")
+        print("Warning: 'port' uses a well-known port")
         print("Your Elasticsearch node may conflict with other services")
-        baseEndpoint = baseEndpoint + str(argdict['http-port'])
+        baseEndpoint = baseEndpoint + str(argdict['port'])
     else:
-        baseEndpoint = baseEndpoint + str(argdict['http-port'])
+        baseEndpoint = baseEndpoint + str(argdict['port'])
 
     endpoint = baseEndpoint
 
--- valp.py
+++ valp.py
@@ -1,13 +1,17 @@
-import socket
+try:
+    import ipaddress
+except ImportError:
+    print("elastictl requires 'ipaddress'")
+    print("Please run 'pip3 install ipaddress' before proceeding")
 
-# Stolen from https://stackoverflow.com/questions/319279/how-to-validate-ip-address-in-python
 def validateIPv4(input):
     try:
-        socket.inet_pton(socket.AF_INET, address)
-    except:
+         ipaddress.IPv4Address(input)
+    except AddressValueError:
+        print("Possible improperly formatted IP address")
         return False
-    
+
     return True
-    
+
 def validateIPv6(input):
     return False
