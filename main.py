6b76a8b5e97205e160a9189cf9adde04bfcd8922
Claire Beamer <clairebea@protonmail.com>
2019-05-16 21:14:39 -0600

    Base framework and working docker-compose.

--- main.py
+++ main.py
@@ -1,12 +1,113 @@
 #!/usr/bin/env python3
 
+# We try to stay as close to standard library as possible.
+import sys
+import json
+import os
+
+# Do we have the things we need?
+
+try:
+    import argparse
+except ImportError:
+    print("elastictl requires argparse")
+    print("Please run 'pip3 install argparse' before proceeding")
+    sys.exit(1)
+
+try:
+    import requests
+except ImportError:
+    print("elastictl requires requests")
+    print("Please run 'pip3 install requests' before proceeding")
+    sys.exit(1)
+
+# Script specific utilities
+# valp has the validate functions
+from valp import *
+
+
 def main():
-    return False
+    argdict = {}
+    # Start the argument parser
+    parser = argparse.ArgumentParser()
+    # Set the cluster endpoint i.e. localhost
+    parser.add_argument("--addr", help="The address of the Elasticsearch \
+                        host. Defaults to '127.0.0.1'. Currently only supports \
+                        IP addresses", action="store_true")
+    # Test the cluster endpoint, check if we can hit the specified port
+    parser.add_argument("--test", help="Checks if the given URL and port can \
+                        be reached and outputs some helpful data" )
+    # Set the es port we will be using, default of 9200
+    parser.add_argument("--http-port", help="The HTTP port of the \
+                         Elasticsearch host. Defaults to 9200", \
+                        action="store_true")
+    
+    args = parser.parse_args()
 
+    if args.test:
+        argdict['test'] = True
+    else: 
+        argdict['test'] = False
 
-def gatherArgs():
-    return False
+    if args.addr:
+        argdict['addr'] = args.addr
+    else:
+        argdict['addr'] = "localhost"
+
+    if args.http-port:
+        argdict['http-port'] = args.http-port
+    else:
+        argdict['http-port'] = 9200
+    endpoint = endPointConstructor(argdict)
+
+    print(endpoint)
+
+# Construct the endpoint and parameters we want to hit
+def endPointConstructor(argdict):
+    # Currently Elasticsearch only supports API calls 
+    # over http.
+    baseProto = "http"
+    if type(argdict) == "<class 'dict'>":
+        pass
+    else:
+        print("Something went wrong. The parameters were incorrectly formatted")
+        sys.exit(1)
+
+    validIP = validateIPs(argdict['addr'])
+
+    if validIP == True:
+        baseEndpoint = baseProto + argdict['addr']
+    else:
+        print("Possible improperly formatted IP address")
+        print("Exiting")
+        sys.exit(1)
+    
+    if argdict['http-port'] == 9200:
+        pass
+    elif argdict['http-port'] <= 1024: 
+        # Log warning the Elasticsearch port we're connecting to 
+        # conflicts with other ports.
+        print("Warning: 'http-port' uses a well-known port")
+        print("Your Elasticsearch node may conflict with other services")
+        baseEndpoint = baseEndpoint + str(argdict['http-port'])
+    else:
+        baseEndpoint = baseEndpoint + str(argdict['http-port'])
+
+    endpoint = baseEndpoint
+
+    return endpoint
+
+# Map all possible cluster settings and parameters here
+class clusterStatus():
+    # Get the current cluster master
+    def clusterMaster(self, endpoint):
+        pass
+
+# Check the status of your indices here
+class indexStatus():
+    # Return a list of all indices
+    def indexList(self, endpoint):
+        pass
     
 if __name__ == '__main__':
     main()
-
