b8fc96803af6e008ddbd43e4d36dc304a2f77c64
Beamer <cbeam@/dev/null>
2020-01-15 17:53:28 -0700

    Base functionality. Running main.py will check if Elasticsearch is up

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ import os
 
 try:
     import argparse
-except ImportError:
+except ModuleNotFoundError:
     print("elastictl requires argparse")
     print("Please run 'pip3 install argparse' before proceeding")
     sys.exit(1)
 
 try:
-    import requests
-except ImportError:
-    print("elastictl requires requests")
-    print("Please run 'pip3 install requests' before proceeding")
+    import httpx
+except ModuleNotFoundError:
+    print("elastictl requires httpx")
+    print("Please run 'pip3 install httpx' before proceeding")
     sys.exit(1)
 
 # Script specific utilities
@@ -45,34 +45,60 @@ def main():
     # Set the es port we will be using, default of 9200
     parser.add_argument("--port", type=int, default=9200, help="The HTTP port of the\
                          Elasticsearch host. Defaults to 9200")
-    
-    args = parser.parse_args()
-    f = EndPointConstructor(args)
-    print(f)
-# Construct a valid endpoint here, either composed of an
-# IP address with an appropriate protocol and
-# port utilized.
 
-class EndPointConstructor():
+    parser.add_argument("--verbose", help="Enable verbose output of data",
+                        action="store_true")
+
+    # Build out args object
+    args = parser.parse_args()
+    # Are we verbose
+    if args.verbose:
+      verbose = args.verbose
+    else:
+      verbose = False
+
+    # Construct out baseurl from the BaseURL class.
+    baseurl = BaseURL(args)
+
+    # Default action here: simply see if we hit an ES endpoint
+    if args.test:
+        pass
+    else:
+      status = EndpointValues(baseurl, verbose).node_status()
+      print(status)
+
+class BaseURL():
     def __init__(self, args):
         self.protocol = args.protocol
         self.addr = args.addr
         self.port = args.port
-
+        self.baseurl = f"{self.protocol}://{self.addr}:{self.port}"
     def __repr__(self):
         return f"{self.protocol}://{self.addr}:{self.port}"
 
-# Map all possible cluster settings and parameters here
-class ClusterStatus():
-    # Get the current cluster master
-    def cluster_settings(self, endpoint):
+class EndpointValues():
+    def __init__(self, baseurl, verbose):
+        self.url = baseurl.baseurl
+        self.verbose = verbose
+    def cluster_settings(self):
         pass
 
-# Check the status of your indices here
-class IndexStatus():
     # Return a list of all indices
-    def index_list(self, endpoint):
+    def index_list(self):
         pass
-    
+
+    def node_status(self):
+        es_status = httpx.get(self.url)
+        if es_status.status_code != 200:
+            return f"Error: Recieved: { es_status.status_code }"
+        else:
+            if self.verbose == True:
+                # Get the full data from the endpoint
+                return es_status.text
+            elif self.verbose == False:
+                # We just want to know if the cluster is working.
+                es_json = json.loads(es_status.text)
+                return f"Elasticsearch is up! Node: { eses__json['name'] }"
+
 if __name__ == '__main__':
     main()
