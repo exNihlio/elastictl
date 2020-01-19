b184679b4cc9ac11c4f52614c7a7f51f690ac499
Beamer <cbeam@/dev/null>
2020-01-18 19:44:18 -0700

    Add in type checking so we can check which sub-command has been used

--- cli.py
+++ cli.py
@@ -19,18 +19,25 @@ def parser():
     parser.add_argument("--verbose", help="Enable verbose output of data",
                         action="store_true")
 
+    parser.add_argument("--test", default=False, help="Test to see if Elasticsearch is up",
+                        action="store_true")
+
     subparsers = parser.add_subparsers(help="REST API equivalent sub-commands")
     # Create the parser for index related functions
     index_parser = subparsers.add_parser("index", help="Index related functions")
+    # This allows us to see which sub-command we've accessed
+    index_parser.set_defaults(which='index')
     index_parser.add_argument("--name", type=str, help="Name of the index", required=True)
     index_group = index_parser.add_mutually_exclusive_group()
-    index_group.add_argument("--get", type=str, help="Get an index")
-    index_group.add_argument("--delete", type=str, help="Delete an index")
-    index_group.add_argument("--create", type=str, help="Create an index")
-    index_group.add_argument("--exists", type=str, help="Check if an index exists")
+    index_group.add_argument("--get", action="store_true", help="Get an index")
+    index_group.add_argument("--delete", action="store_true", help="Delete an index")
+    index_group.add_argument("--create", action="store_true", help="Create an index")
+    index_group.add_argument("--exists", action="store_true", help="Check if an index exists")
 
     # Create the parser for node related functions
     nodes_parser = subparsers.add_parser("node", help="Node related functions")
+    # This allows us to see which sub-command we've accessed
+    nodes_parser.set_defaults(which='node')
     nodes_parser.add_argument("--name", type=str, help="Name of the node")
     nodes_group = nodes_parser.add_mutually_exclusive_group()
     nodes_group.add_argument("--hot_threads", type=str, help="Return hot threads on specified node")
@@ -40,6 +47,8 @@ def parser():
     # Create the parser for cluster related functions
     cluster_parser = subparsers.add_parser("cluster", help="Cluster related functions")
     cluster_group = cluster_parser.add_mutually_exclusive_group()
+    # This allows us to see which sub-command we've accessed
+    cluster_parser.set_defaults(which='cluster')
     cluster_group.add_argument("--health", type=str, help="Return the cluster health")
     cluster_group.add_argument("--state", type=str, help="Return the cluster state")
     cluster_group.add_argument("--stats", type=str, help="Return stats of the cluster")
